168acded0c858cf43fae4df1f04ee2b8a6e8b08a
Phung Vu Tran Kim <vtkp2002dn@gmail.com>
2019-12-02 13:39:05 +0700

    add service fetch heroes data

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../../classes/hero";
 import {HEROES} from "../../data/mock-heroes";
+import {HeroService} from "../../services/hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,17 @@ import {HEROES} from "../../data/mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
-  constructor() { }
+  heroes: Hero[]
+  selectedHero: Hero
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
-  selectedHero: Hero
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
