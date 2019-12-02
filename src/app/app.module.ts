e9c3d4a18598e79ea175b3c0ab07d2df62254c34
Phung Vu Tran Kim <vtkp2002dn@gmail.com>
2019-12-02 10:29:04 +0700

    create hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
